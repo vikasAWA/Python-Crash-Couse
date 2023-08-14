20a07f4eb6abbac22629cf2d56171d5f06dbf0cd
vikasAWA <awasthivikas02@gmail.com>
2023-08-14 11:41:17 +0530

    Extended Restaurant and User files

--- restaurant.py
+++ restaurant.py
@@ -4,7 +4,7 @@ class Restaurant():
         """Initializing restaurant_name and cuisine_type attributes."""
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
-        
+        self.number_served = 0
     def describe_restaurant(self):
         """Describing restaurant"""
         print(f"Name of restaaurant is {self.restaurant_name.title()}.\nCuisine Type is {self.cuisine_type.title()}.\n")
@@ -13,13 +13,32 @@ class Restaurant():
         """Printing mesage indicating the status of restaurant open or not"""
         print(f"{self.restaurant_name.title()} is open.\n")
         
+    def set_number_served(self, number):
+        if number >= self.number_served:
+            self.number_served = number
+        else:
+            print("You can not decrease the number of peple served. !")
+            
+    def increment_number_served(self, number):
+        """Add the given number to the existing number number served"""
+        if number >= 0:
+            self.number_served += number
+        
 restaurant = Restaurant("Delhi food express", "North Indian food")
-print(restaurant.restaurant_name)
-print(restaurant.cuisine_type)
+#print(restaurant.restaurant_name)
+#print(restaurant.cuisine_type)
 restaurant.describe_restaurant()
 restaurant.open_restaurant()
 
-restaurant_1 = Restaurant("Chennai Foods", "South indian")
+"""restaurant_1 = Restaurant("Chennai Foods", "South indian")
 restaurant_2 = Restaurant("Punjabi Dhaba", "north Indian")
 restaurant_1.describe_restaurant()
-restaurant_2.describe_restaurant()
+restaurant_2.describe_restaurant()"""
+
+print(f"Number of Customer Served : {restaurant.number_served}")
+restaurant.number_served = 10
+print(f"Number of Customer Served : {restaurant.number_served}")
+restaurant.set_number_served(20)
+print(f"Number of Customer Served : {restaurant.number_served}")
+restaurant.increment_number_served(5)
+print(f"Number of Customer Served : {restaurant.number_served}")
--- user.py
+++ user.py
@@ -7,6 +7,7 @@ class User():
         self.age = age 
         self.location = location
         self.occupation = occupation
+        self.login_attempts = 0
         
     def describe_user(self):
         print(f"User: {self.first_name.title()} {self.last_name.title()}")
@@ -17,9 +18,28 @@ class User():
     def greet_user(self):
         print(f"Hello, {self.first_name}! Welcome back!")
         
-user_1 = User("john", 'doe', 20, 'newyork', 'software engineer')
+    def increment_login_attempts(self):
+        """Increment the login_attempts by 1"""
+        self.login_attempts += 1
+        
+    def reset_login_attempts(self):
+        """Reset the login attempts to 0"""
+        self.login_attempts = 0
+
+"""user_1 = User("john", 'doe', 20, 'newyork', 'software engineer')
 user_1.describe_user()
 user_1.greet_user()
 user_2 = User("robert", 'parell', 40, 'San fransisco', 'manager')
 user_2.describe_user()
 user_2.greet_user()
+"""
+
+user = User("Ram", "jane", 23, "Delhi", "data analyst")
+user.describe_user()
+print(f"Login attempts: {user.login_attempts}")
+user.increment_login_attempts()
+print(f"Login attempts: {user.login_attempts}")
+user.increment_login_attempts()
+print(f"Login attempts: {user.login_attempts}")
+user.reset_login_attempts()
+print(f"Login attempts: {user.login_attempts}")
